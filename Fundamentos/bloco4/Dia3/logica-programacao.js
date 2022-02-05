fcb890ec34fc586e9daa5be85630d70daa0dc66f
LaisMassacesi <laismassacesi@gmail.com>
2022-02-05 18:22:32 -0300

    exercicios - logica de programação / cria arquivo exercicio-bonus

---
+++ Fundamentos/bloco4/Dia3/logica-programacao.js
@@ -0,0 +1,85 @@
+
+// numeros = 0
+// let fruits = [3, 4, 10, 1, 12];
+// for (let i = 0 ; i < fruits.length ; i += 1){
+//     numeros = fruits[i] + numeros
+// }
+// if (numeros > 15){
+//     console.log(numeros)
+// }
+// else{
+//     console.log("valor menor que 16")
+// }
+
+// console.log(numeros)
+
+
+// gaveta = 0 
+// let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for(let c = 10; c < numero.length; c -= 1){
+//     gaveta = numero[c]
+//     console.log(gaveta)
+
+// }
+
+// O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero.
+
+// let fatorial = 1
+// for(let c = 10; c > 0; c -= 1){
+//     fatorial *= c
+// }
+// console.log(fatorial)
+
+
+// let word = 'tryber'
+// let reserveWord = ''
+// for(let c = 5; c >= 0; c -= 1){
+//     reserveWord = word[c]
+//     console.log(reserveWord)
+// }
+
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let reserveWord = ''
+// let bigger = ''
+// for(let c = 0; c < array.length; c += 1){
+//     reserveWord = array[c]
+//     if(reserveWord >= bigger[c]){
+//         bigger = reserveWord
+//     }
+//     console.log(bigger)
+
+
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra. 
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// menorPalavra = array[0]; 
+// maiorPalavra = array[0];
+
+// for(let c = 0; c < array.length; c += 1){
+//     if(array[c].length > maiorPalavra.length){
+//         maiorPalavra = array[c]
+//     }
+// }
+// for(let c = 0; c < array.length; c += 1){
+//     if(array[c].length < menorPalavra.length){
+//         menorPalavra = array[c]
+//     }
+// }
+// console.log("maior palavra: " + maiorPalavra)
+// console.log("menor palavra: " + menorPalavra)
+
+
+
+// Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+
+let cousinNumb = 0
+let reserveNumb = 0
+for(let index = 0; index < 50; index += 1){
+    if()
+}
+
+
+
+
